f8ce2c41e2ce35a59c618414d1eb439345da0b1f
VasyaSmolyar <smolyar4@gmail.com>
2019-08-24 22:22:15 +0300

    Fix write to CSV

--- script.py
+++ script.py
@@ -21,60 +21,53 @@ film_urls = catalogParsedPage.xpath('//td[@class="news"]/div/a/@href')
 #Находит ссылки на страницы фильмов фильмы
 print(film_urls)
 
-for film_page in film_urls:
-    film_page_url = "https://www.kinopoisk.ru" + film_page + "reviews/?ord=rating"
-    filmParsedPage = getParsedPage(film_page_url)
-    
-    reviews_texts = filmParsedPage.xpath('//span[@itemprop="reviewBody"]')
-    print(reviews_texts)
-    reviews_marks = filmParsedPage.xpath('//div[@itemprop="reviews"]/@class')
-    print(reviews_marks)
-    
-    reviews = []
-    
-    for i in reviews_texts:
-        index = reviews_texts.index(i)
-        print(index)
-        reviews_text = ""
-        
-        text_parts = i.xpath('text()')
-        print(text_parts)
-        
-        for t in text_parts:
-            reviews_text += t
-            print(t)
-        
-        literals = reviews_text.split('"')
-        review = ""
-        for l in literals:
-            review = review + l + "\""
+with open('index.csv', 'w') as csvFile:
+
+    try:
+
+        for film_page in film_urls:
+            film_page_url = "https://www.kinopoisk.ru" + film_page + "reviews/?ord=rating"
+            filmParsedPage = getParsedPage(film_page_url)
             
-        print(review)
+            reviews_texts = filmParsedPage.xpath('//span[@itemprop="reviewBody"]')
+            print(reviews_texts)
+            reviews_marks = filmParsedPage.xpath('//div[@itemprop="reviews"]/@class')
+            print(reviews_marks)
             
-        review = '\"' + review
-        
-        grade = "1"
-        
-        if reviews_marks[index] == "response good":
-            grade = "2"
-        elif reviews_marks[index] == "response neutral":
-            grade = "1"
-        else:
-            grade = "0"
+            reviews = []
             
-        newArray = [review, grade]
-        print(newArray)
-        
-        with open('index.csv', 'w') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerows(newArray)
-        csvFile.close()
-        
+            for i in reviews_texts:
+                index = reviews_texts.index(i)
+                print(index)
+                reviews_text = ""
+                
+                text_parts = i.xpath('text()')
+                print(text_parts)
+                
+                for t in text_parts:
+                    reviews_text += t
+                    print(t)
+                
+                review = reviews_text.replace("\"","'")
+                review = review.replace("\r\n"," ")
+                review = review.replace("\n"," ")
+                print(review)
+                    
+                grade = "1"
+                
+                if reviews_marks[index] == "response good":
+                    grade = "2"
+                elif reviews_marks[index] == "response neutral":
+                    grade = "1"
+                else:
+                    grade = "0"
+                    
+                newArray = [review, grade]
+                print(newArray)
+                
+                csvFile.write('"{}",{}\n'.format(review, grade))
         
         time.sleep(60)
-    
-    time.sleep(60)
 
-    q = "\""
-    
-time.sleep(60)
+    except KeyboardInterrupt:
+        pass
